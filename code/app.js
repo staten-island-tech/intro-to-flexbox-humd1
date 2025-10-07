d8a6fa754bee5a9848eae55cc4c4e08eae0b804b
humd1 <humdb@nycstudents.net>
2025-10-07 12:30:55 -0400

    ok

---
+++ code/app.js
@@ -0,0 +1,6 @@
+const DOMSelectors = {
+  title: document.getElementById("title"),
+  artist: document.getElementById("artist"),
+  url: document.getElementById("url"),
+  display: document.getElementById("display"),
+};
