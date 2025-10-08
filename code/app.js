0a384559da2177dc36c323e657ad8222828ba4d8
humd1 <humdb@nycstudents.net>
2025-10-08 12:32:27 -0400

    oik

--- code/app.js
+++ code/app.js
@@ -1,6 +1,105 @@
-const DOMSelectors = {
-  title: document.getElementById("title"),
-  artist: document.getElementById("artist"),
-  url: document.getElementById("url"),
-  display: document.getElementById("display"),
-};
+const jewelry = [
+    {
+        name: "Chain",
+        price: "$59.00",
+        inStock: true,
+        brand: "Dorado",
+        img: "",
+        alt:"",
+    },
+    {
+        name: "PLACEHOLDER",
+        price: "$##.##",
+        inStock: true,
+        brand: "PLACEHOLDER",
+        img: "",
+        alt:"",
+    },
+]
+
+const tops = [
+    {
+        name: "Parka",
+        price: "$49.90",
+        inStock: true,
+        brand: "Uniqlo",
+        img: "",
+        alt:"",
+    },
+    {
+        name: "AIRISM shirt",
+        price: "$##.##",
+        inStock: true,
+        brand: "Uniqlo",
+        img: "",
+        alt:"",
+    },
+    {
+        name: "T-Shirt",
+        price: "$##.##",
+        inStock: true,
+        brand: "Aeropostale",
+        img: "",
+        alt:"",
+    },
+
+]
+
+const pants = [
+    {
+        name: "Denim Jeans",
+        price: "$40.00",
+        inStock: true,
+        brand: "Old Navy",
+        img: "",
+        alt:"",
+    },
+    {
+        name: "PLACEHOLDER",
+        price: "$##.##",
+        inStock: true,
+        brand: "PLACEHOLDER",
+        img: "",
+        alt:"",
+    },
+]
+
+
+const bags = [
+    {
+        name: "PLACEHOLDER",
+        price: "$##.##",
+        inStock: true,
+        brand: "PLACEHOLDER",
+        img: "",
+        alt:"",
+    },
+    {
+        name: "PLACEHOLDER",
+        price: "$##.##",
+        inStock: true,
+        brand: "PLACEHOLDER",
+        img: "",
+        alt:"",
+    },
+]
+
+
+const shoes = [
+    {
+        name: "PLACEHOLDER",
+        price: "$##.##",
+        inStock: true,
+        brand: "PLACEHOLDER",
+        img: "",
+        alt:"",
+    },
+    {
+        name: "PLACEHOLDER",
+        price: "$##.##",
+        inStock: true,
+        brand: "PLACEHOLDER",
+        img: "",
+        alt:"",
+    },
+]
