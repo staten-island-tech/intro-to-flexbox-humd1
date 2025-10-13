ad9fa4d4fde422fb82dca104cd423defbc8f75e3
humd1 <humdb@nycstudents.net>
2025-10-13 13:58:09 -0400

    RESTARTING

--- code/app.js
+++ code/app.js
@@ -1,105 +1,122 @@
-const jewelry = [
-    {
-        name: "Chain",
-        price: "$59.00",
-        inStock: true,
-        brand: "Dorado",
-        img: "",
-        alt:"",
-    },
-    {
-        name: "PLACEHOLDER",
-        price: "$##.##",
-        inStock: true,
-        brand: "PLACEHOLDER",
-        img: "",
-        alt:"",
-    },
-]
-
-const tops = [
-    {
-        name: "Parka",
-        price: "$49.90",
-        inStock: true,
-        brand: "Uniqlo",
-        img: "",
-        alt:"",
-    },
-    {
-        name: "AIRISM shirt",
-        price: "$##.##",
-        inStock: true,
-        brand: "Uniqlo",
-        img: "",
-        alt:"",
-    },
-    {
-        name: "T-Shirt",
-        price: "$##.##",
-        inStock: true,
-        brand: "Aeropostale",
-        img: "",
-        alt:"",
-    },
-
-]
-
-const pants = [
-    {
-        name: "Denim Jeans",
-        price: "$40.00",
-        inStock: true,
-        brand: "Old Navy",
-        img: "",
-        alt:"",
-    },
-    {
-        name: "PLACEHOLDER",
-        price: "$##.##",
-        inStock: true,
-        brand: "PLACEHOLDER",
-        img: "",
-        alt:"",
-    },
-]
-
-
-const bags = [
-    {
-        name: "PLACEHOLDER",
-        price: "$##.##",
-        inStock: true,
-        brand: "PLACEHOLDER",
-        img: "",
-        alt:"",
-    },
-    {
-        name: "PLACEHOLDER",
-        price: "$##.##",
-        inStock: true,
-        brand: "PLACEHOLDER",
-        img: "",
-        alt:"",
-    },
-]
-
-
-const shoes = [
-    {
-        name: "PLACEHOLDER",
-        price: "$##.##",
-        inStock: true,
-        brand: "PLACEHOLDER",
-        img: "",
-        alt:"",
-    },
-    {
-        name: "PLACEHOLDER",
-        price: "$##.##",
-        inStock: true,
-        brand: "PLACEHOLDER",
-        img: "",
-        alt:"",
-    },
-]
+const products = [
+  {
+    name: "Uniqlo Parka",
+    description: "The Favorite Jacket",
+    price: "$49.90",
+    img: "jacket.PNG",
+    alt: "Uniqlo Parka",
+    category: "tops"
+  },
+  {
+    name: "New Balance 530s",
+    description: "The Most Used Shoe",
+    price: "$109.99",
+    img: "shoe.PNG",
+    alt: "New Balance 530s",
+    category: "shoes"
+  },
+  {
+    name: "Aeropostale Shirts",
+    description: "The go-to home shirt",
+    price: "$32.95",
+    img: "aero.PNG",
+    alt: "Aeropostale Shirt",
+    category: "tops"
+  },
+  {
+    name: "Uniqlo AIRISM Shirts",
+    description: "Home shirt part 2",
+    price: "$19.90",
+    img: "uniqlo.PNG",
+    alt: "Uniqlo AIRISM Shirt",
+    category: "tops"
+  },
+  {
+    name: "Michael Kors Purse",
+    description: "Because women don't get good pockets",
+    price: "$118.50",
+    img: "purse.PNG",
+    alt: "Michael Kors Purse",
+    category: "bags"
+  },
+  {
+    name: "Cool Chain",
+    description: "My mom got this for free somehow",
+    price: "$59.00",
+    img: "chain.PNG",
+    alt: "Cool Chain",
+    category: "jewelry"
+  },
+  {
+    name: "Hijab",
+    description: "Nothing but black",
+    price: "$20.00",
+    img: "hijab.jpg",
+    alt: "Hijab",
+    category: "tops"
+  },
+  {
+    name: "Kate Spade Heart Earrings",
+    description: "I lost the other ones",
+    price: "$35.00",
+    img: "heartearrings.PNG",
+    alt: "Kate Spade Heart Earrings",
+    category: "jewelry"
+  },
+  {
+    name: "Kate Spade Huggies",
+    description: "Only real gold",
+    price: "$38.00",
+    img: "earrings.PNG",
+    alt: "Kate Spade Huggies",
+    category: "jewelry"
+  },
+  {
+    name: "Jansport Backpack",
+    description: "Pain in the back",
+    price: "$44.00",
+    img: "bag.png",
+    alt: "Jansport Backpack",
+    category: "bags"
+  },
+  {
+    name: "Hoodie",
+    description: "Comes off after first period",
+    price: "$30.00",
+    img: "hoodie.PNG",
+    alt: "Hoodie",
+    category: "tops"
+  },
+  {
+    name: "Name Chain",
+    description: "Somehow every brown person has one",
+    price: "$65.00",
+    img: "name.PNG",
+    alt: "Name Chain",
+    category: "jewelry"
+  },
+  {
+    name: "Old Navy Jeans",
+    description: "An outfit repeater's dream",
+    price: "$40.00",
+    img: "jeans.PNG",
+    alt: "Old Navy Jeans",
+    category: "pants"
+  },
+  {
+    name: "Guess Bag",
+    description: "The retired purse",
+    price: "$50.00",
+    img: "retiredbag.PNG",
+    alt: "Guess Bag",
+    category: "bags"
+  },
+  {
+    name: "GAP Cargo Jeans",
+    description: "Woah pockets",
+    price: "$44.00",
+    img: "gapjeans.PNG",
+    alt: "GAP Jeans",
+    category: "pants"
+  },
+];
